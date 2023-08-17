9407a1dd2037f5dd7b6d628759f82d3edb6add22
Siddharth <chavansiddharth.54@gmail.com>
2023-08-17 21:04:19 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "empp.cid3pmyezwuy.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "siddharth"
+customdb = "empp"
+custombucket = "addempsp"
+customregion = "ap-south-1"
